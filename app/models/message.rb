cd387fe207b79338bb63deb475922ba0260a9e17
shiki-k-1225 <4949gtmw@gmail.com>
2024-11-25 18:54:50 +0900

    各モデルのテスト

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,9 +3,9 @@ class Message < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :content, presence: true, unless: :was_attached?
+  validates :content, presence: true, unless: :attached?
 
-  def was_attached
-    self.image.was_attached
+  def attached?
+    self.image.attached?
   end
 end
